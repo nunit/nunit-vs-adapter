8dfb46c7d1e0042c1153f8ac16725d8ad0cbc35e
Charlie Poole <charliepoole@gmail.com>
2016-12-15 16:10:25 -0800

    Resolve conflicts in copyright statements

--- src/NUnitTestAdapter/Properties/AssemblyInfo.cs
+++ src/NUnitTestAdapter/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("NUnit Software")]
 [assembly: AssemblyProduct("NUnit.VisualStudio.TestAdapter")]
-[assembly: AssemblyCopyright("Copyright 2011-2017, NUnit Software")]
+[assembly: AssemblyCopyright("Copyright (c) 2011-2015, Charlie Poole")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(false)]
